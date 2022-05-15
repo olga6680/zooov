e9a40b5a4d0abeb332ccadf7a67997eef2d1bd3f
Olga <boa906680@gmail.com>
2022-05-15 10:07:29 +0300

    end section Promo

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('.header'),
+        menuItem = document.querySelectorAll('.header__menu_item'),
         hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
         header.classList.toggle('header_active');
     });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            header.classList.toggle('header_active');
+        })
+    })
 });
