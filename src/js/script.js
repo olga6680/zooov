fd360d68d99962ed5af803d9cdec8fc9a72d05b1
Olga <boa906680@gmail.com>
2022-05-15 12:27:34 +0300

    chenge header with scroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('.header'),
         menuItem = document.querySelectorAll('.header__menu_item'),
-        hamburger = document.querySelector('.hamburger');
+        hamburger = document.querySelector('.hamburger'),
+        scrollHeader = document.querySelector('.header');
 
     hamburger.addEventListener('click', () => {
         header.classList.toggle('header_active');
@@ -12,4 +13,12 @@ window.addEventListener('DOMContentLoaded', () => {
             header.classList.toggle('header_active');
         })
     })
+
+    window.onscroll = function() {
+        if (window.pageYOffset > 122) {
+            scrollHeader.classList.add('header__scroll');
+        } else if (window.pageYOffset < 122) {
+            scrollHeader.classList.remove('header__scroll');
+        }
+    }
 });
